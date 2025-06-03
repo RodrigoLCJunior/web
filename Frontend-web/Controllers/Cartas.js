d259b573a5f1277497b4c439012edbf557cba651
Angelozaa <145221675+Angelozaa@users.noreply.github.com>
2025-06-03 16:03:02 -0300

    Junção de frontends
    Atualização para juntas os frontends dos anteriores em um só

---
+++ Frontend-web/Controllers/Cartas.js
@@ -0,0 +1,43 @@
+import { UserService } from '../Services/user_service.js';
+
+const cardsContainer = document.getElementById('cards');
+const usuario = UserService.getCurrentUser();
+
+if (usuario && usuario.avatar && Array.isArray(usuario.avatar.deck) && usuario.avatar.deck.length > 0) {
+  let cardsHTML = '';
+
+  usuario.avatar.deck.forEach(card => {
+    cardsHTML += `
+      <div class="card">
+      <img src="/Assets/images/${card.imageCard}" alt="${card.nome}">
+      <div class="card-details">
+        <h3>${card.nome}</h3>
+        <p><strong>Descrição:</strong> ${card.descricao}</p>
+        <p><strong>Valor:</strong> ${card.valor}</p>
+        <p><strong>Efeito:</strong> ${card.tipoEfeito}</p>
+      </div>
+    </div>
+  `;
+});
+
+  cardsContainer.innerHTML = cardsHTML;
+
+  const cards = document.querySelectorAll('.card');
+
+  cards.forEach(card => {
+    card.addEventListener('click', () => {
+      // Se a carta já está expandida, fecha ela
+      if (card.classList.contains('expanded')) {
+        card.classList.remove('expanded');
+      } else {
+        // Fecha todas as outras cartas
+        cards.forEach(c => c.classList.remove('expanded'));
+        // Expande a clicada
+        card.classList.add('expanded');
+      }
+    });
+  });
+
+} else {
+  cardsContainer.innerHTML = '<p>Você ainda não possui cartas no seu deck.</p>';
+}
